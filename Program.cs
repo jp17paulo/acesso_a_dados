3493a8cdab12b1911909b2e178976b08ee5ff95d
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2021-12-22 23:50:02 -0300

    SqlConnection

--- Program.cs
+++ Program.cs
@@ -7,8 +7,22 @@ namespace BaltaDataAccess
     {
         static void Main(string[] args)
         {
-            const string connectionString
-            = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";         
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";  
+            
+            // var connection = new SqlConnection();
+            // connection.Open();
+            
+            // //INSERT
+            // //UPDATE
+
+            // connection.Close();
+            // connection.Dispose();
+            
+            using(var connection = new SqlConnection())
+            {
+                Console.WriteLine("Conectado");
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
