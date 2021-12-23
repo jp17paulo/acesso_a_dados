6e5b76e7945fa5a26cfc27f0cde0c8727812a552
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2021-12-23 00:14:42 -0300

    SqlCommand e SqlDataReader

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace BaltaDataAccess
     {
         static void Main(string[] args)
         {
-            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";  
+            // const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$"; 
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;"; 
             
             // var connection = new SqlConnection();
             // connection.Open();
@@ -18,9 +19,20 @@ namespace BaltaDataAccess
             // connection.Close();
             // connection.Dispose();
             
-            using(var connection = new SqlConnection())
+            using(var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Conectado");
+                connection.Open();
+                using(var command = new SqlCommand()){
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [ID], [Title]  FROM [Category]";
+
+                    var reader =  command.ExecuteReader();
+                    while(reader.Read()){
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
             }
 
             Console.WriteLine("Hello World!");
