ffa89516eb0b1e7a8d60aaa37e7ad27537f9731a
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2022-02-02 12:58:05 -0300

    Criação de insert com parametros

--- Program.cs
+++ Program.cs
@@ -25,21 +25,21 @@ namespace BaltaDataAccess
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
-            category.URL = "amazon";
+            category.Url = "amazon";
             category.Description = "Categoria destinada a serviços do AWS";
             category.Order = 8;
             category.Summary = "AWS Cloud";
-            category.Feature = false;
-            var insertSql = @"INSERT INTO ]
+            category.Featured = false;
+            var insertSql = @"INSERT INTO
                     [Category] 
             VALUES(
-                NEWID(), 
-                title, 
-                url, 
-                summary, 
-                order, 
-                description, 
-                featured)";  
+                @Id, 
+                @Title, 
+                @Url, 
+                @Summary, 
+                @Order, 
+                @Description, 
+                @Featured)";  
 
             using(var connection = new SqlConnection(connectionString))
             {
@@ -57,14 +57,27 @@ namespace BaltaDataAccess
                 //     }
                 // }
                 #endregion
-              
-              var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-              foreach(var item in categories){
-                  Console.WriteLine($"{item.Id} - {item.Title}");
-              }
-            }
 
-            Console.WriteLine("Hello World!");
+                var rows = connection.Execute(insertSql, new {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                }); 
+                
+                Console.WriteLine($"{rows} linhas inseridas");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                
+                foreach(var item in categories){
+                    Console.WriteLine($"{item.Id} - {item.Title}");
+                  }
+                }
+
+                Console.WriteLine("Hello World!");
 
             #endregion
             
